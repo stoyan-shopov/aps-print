d04e0f11c08e64aae028e184867289d166679efe
Stoyan Shopov <stoyan.shopov@gmail.com>
2016-07-12 18:58:12 +0300

    initial usb bus enumeration code added

--- app/src/main/java/com/example/shopov/aps_print/MainActivity.java
+++ app/src/main/java/com/example/shopov/aps_print/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.shopov.aps_print;
 
+import android.content.Context;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     }
     public void enumerate(View view) {
         Button enumerate_button = (Button)findViewById(R.id.buttonEnumerate);
-        enumerate_button.setText("failed");
+
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+        if (deviceList.isEmpty())
+            enumerate_button.setText("failed");
+        else
+            enumerate_button.setText("enumeration successful" + deviceList.values().toArray()[0]);
     }
 }
