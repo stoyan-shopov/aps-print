3d8fabc34c76e43aefa883d0ef169f8086145a51
Stoyan Shopov <stoyan.shopov@gmail.com>
2016-07-12 18:13:52 +0300

    dummy action added for the 'enumerate' button

--- app/src/main/java/com/example/shopov/aps_print/MainActivity.java
+++ app/src/main/java/com/example/shopov/aps_print/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.shopov.aps_print;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void enumerate(View view) {
+        Button enumerate_button = (Button)findViewById(R.id.buttonEnumerate);
+        enumerate_button.setText("failed");
     }
 }
