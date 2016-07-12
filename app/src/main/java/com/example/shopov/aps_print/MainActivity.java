73bccb43728993b3fd3a3accf9d4ad9171e8f2ee
Stoyan Shopov <stoyan.shopov@gmail.com>
2016-07-12 18:06:16 +0300

    an enumeration button added

--- app/src/main/java/com/example/shopov/aps_print/MainActivity.java
+++ app/src/main/java/com/example/shopov/aps_print/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.shopov.aps_print;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void enumerate(View view) {
     }
 }
